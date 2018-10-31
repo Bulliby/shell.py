00424fa9faa94d9fed5085ab91371c02c29f1640
Guillaume Wells <wellsguillaume@gmail.com>
2018-11-01 00:09:35 +0100

    feat(paser): Unary Operator < & |

--- shell.py
+++ shell.py
@@ -5,8 +5,8 @@
 ###############################################################################
 
 (NAME, NEWLINE, IO_NUMBER, AND_IF, OR_IF, DLESS,  DGREAT,  LESSAND,  
-        GREATAND, OPERATOR) = ('NAME', 'NEWLINE', 'IO_NUMBER', 'AND_IF', 
-'OR_IF', 'DLESS',  'DGREAT',  'LESSAND',  'GREATAND', 'OPERATOR')
+        GREATAND, OPERATOR, PIPE) = ('NAME', 'NEWLINE', 'IO_NUMBER', 'AND_IF', 
+'OR_IF', 'DLESS',  'DGREAT',  'LESSAND',  'GREATAND', 'OPERATOR', 'PIPE')
 
 
 class Token:
@@ -46,9 +46,14 @@ class Lexer:
             "LESSAND"   : "<&",
         } 
 
+        self.u_operator = {
+            "PIPE"      : "|",
+            "AND"       : "&",
+            "LESS"      : "<",
+            "GREAT"     : ">",
+        } 
+
     def error(self):
-        print("%s" % self.current_char)
-        print("%s" % self.peek())
         raise Exception ('Invalid Token')
 
     def advance(self):
@@ -77,14 +82,21 @@ class Lexer:
             result += self.current_char
             self.advance()
 
-        return token(NAME, result.upper())
+        return Token(NAME, result.upper())
 
     def d_operator(self):
         for k, op in self.operators.items():
             if op[0] == self.current_char:
                 if op[1] == self.peek():
-                    return { k : op}
+                    return Token(k, op)
         return None
+
+    def operator(self):
+        for k, op in self.u_operator.items():
+            if op[0] == self.current_char:
+                    return Token(k, op)
+        return None
+
 
 
     def get_next_token(self):
@@ -98,10 +110,15 @@ class Lexer:
 
             #2:: If current char is part of a double operator
             elif self.d_operator():
-                operator = self.d_operator()
+                d_operator = self.d_operator()
                 self.advance()
                 self.advance()
-                return Token(operator.keys(), operator)
+                return d_operator
+
+            elif self.operator():
+                operator = self.operator()
+                self.advance()
+                return operator
 
         #    #3:: If current char is the end of an operator
         #    elif self.current_token.type == OPERATOR and self.if_op_continuation() == 0:
@@ -171,9 +188,6 @@ def main():
             continue
         lexer = Lexer(text)
         token = lexer.get_next_token()
-        if (token != None):
-            #print('first token')
-            print(token.value)
         while token:
             lexer.current_token = Token(None)
             token = lexer.get_next_token()
