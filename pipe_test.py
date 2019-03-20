db84f33d9f8976b4590ddcef2c06ed59af2e8f83
Guillaume Wells <wellsguillaume@gmail.com>
2019-03-20 10:42:48 +0100

    feat(master): Working Poc for pipes

--- pipe_test.py
+++ pipe_test.py
@@ -1,7 +1,8 @@
 import os
 
-def exex_command(cmd1, cmd2, cmd3):
+def exex_command(cmd1, cmd2, cmd3, cmd4):
     r, w = os.pipe()
+    cp_stdout = os.dup(1)
 
     pid = os.fork()
     if pid == 0:
@@ -18,10 +19,18 @@ def exex_command(cmd1, cmd2, cmd3):
         os.dup2(w2, 1)
         os.execvp(cmd2, [cmd2])
 
+    r3, w3 = os.pipe()
     pid3 = os.fork()
     if pid3 == 0:
         os.close(w2)
         os.dup2(r2, 0)
+        os.close(r3)
+        os.dup2(w3, 1)
         os.execvp(cmd3, [cmd3])
 
-exex_command("ls", "wc", "wc")
+    os.close(w3)
+    os.dup2(r3, 0)
+    os.execvp(cmd4, [cmd4])
+
+
+exex_command("ls", "wc", "wc", "wc")
