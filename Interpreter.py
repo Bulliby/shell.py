911e59be4190509bdb76d8dcc45305c39a2b6dbe
Guillaume Wells <wellsguillaume@gmail.com>
2019-05-08 17:54:07 +0200

    seems done

--- Interpreter.py
+++ Interpreter.py
@@ -6,7 +6,7 @@
 #    By: bulliby <wellsguillaume+at+gmail.com>           /   ____/_  _  __     #
 #                                                       /    \  _\ \/ \/ /     #
 #    Created: 2019/03/02 19:56:05 by bulliby            \     \_\ \     /      #
-#    Updated: 2019/05/07 23:57:18 by bulliby             \________/\/\_/       #
+#    Updated: 2019/05/08 17:51:54 by bulliby             \________/\/\_/       #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,6 +25,10 @@ class Interpreter():
             return node.value
         else:
             self.pipe.exec_pipe(self.visit_BinOp(node.left))
+            if self.root is node:
+                os.close(self.pipe.w)
+                os.dup2(self.pipe.r, 0)
+                os.execvp(node.right.value, [node.right.value])
             self.pipe.exec_pipe(self.visit_BinOp(node.right))
             if self.root is node:
                 os.close(self.pipe.w)
