e6d01fcb40acd55c1a4c4ee98c5fa97212f29b2b
Guillaume Wells <wellsguillaume@gmail.com>
2022-04-24 15:49:15 +0200

    feat(master): multiple commands

--- shell.py
+++ shell.py
@@ -4,20 +4,14 @@ from Parser import Cmd
 from Interpreter import Interpreter
 import os
 
-input = input()
-lexer = Lexer(input)
-tokens = lexer.splitInput()
-parser = Parser(tokens)
-node = parser.expr()
-interpreter = Interpreter()
-"""
-if type(node) is Cmd:
-    os.execvp(node.cmd, node.suffix) #Only one CMD
-elif node.token in ['GREAT', 'GREATAND', 'DGREAT'] and type(node.left) is Cmd: # Only a REDIR
-    interpreter.redir.exec_only_redir(
-            interpreter.visit_BinOp(node.left), 
-            interpreter.visit_BinOp(node.right)
-    )
-else:
-"""
-interpreter.visit_BinOp(node)
+str = input()
+while True:
+    lexer = Lexer(str)
+    tokens = lexer.splitInput()
+    parser = Parser(tokens)
+    node = parser.expr()
+    interpreter = Interpreter()
+    interpreter.visit_BinOp(node)
+    str = input()
+    if str == '':
+        break
